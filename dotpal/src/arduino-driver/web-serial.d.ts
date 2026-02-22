8594a63521f4463f6965c8cb672a56c70d1e3f74
Surya Sendhilraj <surya.sendhiraj@gmail.com>
2026-02-22 18:13:44 -0500

    move web serial to driver

---
+++ dotpal/src/arduino-driver/web-serial.d.ts
@@ -0,0 +1,18 @@
+/**
+ * Web Serial API type definitions
+ * Used by ArduinoDriver for serial port communication
+ */
+
+interface SerialPort {
+  open(options: { baudRate: number }): Promise<void>;
+  readable: ReadableStream<Uint8Array>;
+  writable: WritableStream<Uint8Array>;
+  close(): Promise<void>;
+}
+
+interface Navigator {
+  serial: {
+    requestPort(): Promise<SerialPort>;
+    getPorts(): Promise<SerialPort[]>;
+  };
+}
